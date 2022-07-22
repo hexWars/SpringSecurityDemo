8e0a839d207f3d849b01ec47211f2c72c1365b19
HEX <1322423005@qq.com>
2022-07-22 18:14:11 +0800

    feat: 增加webutils

---
+++ src/main/java/top/sehnsucht/springsecuritydemo/utils/WebUtils.java
@@ -0,0 +1,34 @@
+package top.sehnsucht.springsecuritydemo.utils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description:
+ * @Author: Cai
+ * @CreateTime: 2022/7/22
+ */
+
+public class WebUtils {
+    /**
+     * 将字符串渲染到客户端
+     *
+     * @param response 渲染对象
+     * @param string 待渲染的字符串
+     * @return null
+     */
+    public static String renderString(HttpServletResponse response, String string) {
+        try
+        {
+            response.setStatus(200);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            response.getWriter().print(string);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
