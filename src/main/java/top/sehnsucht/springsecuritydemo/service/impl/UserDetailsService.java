c0af883cc52d5436cc75af9ced7bd79bd3946235
HEX <1322423005@qq.com>
2022-07-22 23:11:13 +0800

    feat: 数据库校验登录用户

---
+++ src/main/java/top/sehnsucht/springsecuritydemo/service/impl/UserDetailsService.java
@@ -0,0 +1,38 @@
+package top.sehnsucht.springsecuritydemo.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import top.sehnsucht.springsecuritydemo.entity.LoginUser;
+import top.sehnsucht.springsecuritydemo.entity.User;
+import top.sehnsucht.springsecuritydemo.mapper.UserMapper;
+
+import java.util.Objects;
+
+/**
+ * @Description:
+ * @Author: Cai
+ * @CreateTime: 2022/7/22
+ */
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 查询用户信息
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, username);
+        User user = userMapper.selectOne(queryWrapper);
+        // 如果没有查询到用户
+        if (Objects.isNull(user)) {
+            throw new RuntimeException("用户名或者密码错误");
+        }
+        //todo 查询对应的权限信息
+
+        // 把数据封装成UserDetails
+        return new LoginUser(user);
+    }
+}
