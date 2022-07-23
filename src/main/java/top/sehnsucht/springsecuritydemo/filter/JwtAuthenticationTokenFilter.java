3f860e09ee013cdf2b7e0dbcfab3f392f1095873
HEX <1322423005@qq.com>
2022-07-23 14:45:31 +0800

    feat: token认证过滤器实现

---
+++ src/main/java/top/sehnsucht/springsecuritydemo/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,61 @@
+package top.sehnsucht.springsecuritydemo.filter;
+
+import io.jsonwebtoken.Claims;
+import org.mockito.internal.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import top.sehnsucht.springsecuritydemo.entity.LoginUser;
+import top.sehnsucht.springsecuritydemo.utils.JwtUtil;
+import top.sehnsucht.springsecuritydemo.utils.RedisCache;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @Description:
+ * @Author: Cai
+ * @CreateTime: 2022/7/23
+ */
+
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 获取token
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText("token")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // 解析token
+        String userid;
+        try {
+            Claims claims = JwtUtil.parseJWT(token);
+            userid = claims.getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("token非法");
+        }
+        // 从redis中获取用户信息
+        String redisKey = "login:" + userid;
+        LoginUser loginUser = redisCache.getCacheObject(redisKey);
+        if (Objects.isNull(loginUser)) {
+            throw new RuntimeException("用户未登录");
+        }
+        // 存入SecurityContextHolder
+        //todo 获取权限信息封装到Authentication中
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        // 放行
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/top/sehnsucht/springsecuritydemo/vo/ResponseResult.java
+++ src/main/java/top/sehnsucht/springsecuritydemo/vo/ResponseResult.java
@@ -37,30 +37,6 @@ public class ResponseResult<T> {
         this.data = data;
     }
 
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
     public ResponseResult(Integer code, String msg, T data) {
         this.code = code;
         this.msg = msg;
