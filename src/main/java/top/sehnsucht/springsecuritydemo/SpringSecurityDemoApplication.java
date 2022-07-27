6154dd6a0d8c7d28ab2048dfe0b06d16f0cec6e7
HEX <1322423005@qq.com>
2022-07-27 22:19:32 +0800

    fix: 序列化修复

--- src/main/java/top/sehnsucht/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/top/sehnsucht/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -6,11 +6,6 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @MapperScan("top.sehnsucht.springsecuritydemo.mapper")
@@ -20,13 +15,4 @@ public class SpringSecurityDemoApplication {
         SpringApplication.run(SpringSecurityDemoApplication.class, args);
     }
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(converter);
-    }
 }
--- src/main/java/top/sehnsucht/springsecuritydemo/config/RedisConfig.java
+++ src/main/java/top/sehnsucht/springsecuritydemo/config/RedisConfig.java
@@ -1,25 +1,29 @@
 package top.sehnsucht.springsecuritydemo.config;
 
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.util.TypeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import top.sehnsucht.springsecuritydemo.utils.FastJsonRedisSerializer;
-
-/**
- * @Description:
- * @Author: Cai
- * @CreateTime: 2022/7/22
- */
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     @SuppressWarnings(value = { "unchecked", "rawtypes" })
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
-    {
+    @Autowired(required = false)//降级注入的检测级别
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        //添加白名单 不然会报错！！！！！-------------
+        ParserConfig.getGlobalInstance().addAccept("org.springframework.security.core.authority.");
+        TypeUtils.addMapping("org.springframework.security.core.authority.SimpleGrantedAuthority",
+                SimpleGrantedAuthority.class);
+        //--------------------
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -37,4 +41,3 @@ public class RedisConfig {
         return template;
     }
 }
-
